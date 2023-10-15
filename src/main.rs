ea1345cb6c8720c01819761215e15c2013af2fc0
unknown <err.error.found@gmail.com>
2023-10-15 16:35:22 -0400

    Cleaned up the code a bit

--- src/main.rs
+++ src/main.rs
@@ -5,66 +5,35 @@ fn main()
     manage_first_input();
 }
 
+
 fn manage_first_input()
 {
-    println!("Welcome! Type 'in' (without the quotes) to calculate the number of lattice points in a circle.");
-    println!("Alternatively, type 'circumference' to calculate the number of points on a circle's circumference");
-    let mut first_input = String::new();
+    println!("Welcome! Do you want the circle centered at x = 0 or x = 1/2? (y = 0 for both)");
+    let mut second_input = String::new();
     io::stdin()
-        .read_line(&mut first_input)
+        .read_line(&mut second_input)
         .expect("Failed to read line");
-        
-    println!("{:?}", first_input);
-    if first_input == ("in\r\n")
-    {
-        println!("Do you want the circle centered at x = 0 or x = 1/2? (y = 0 for both)");
-        let mut second_input = String::new();
-        io::stdin()
-            .read_line(&mut second_input)
-            .expect("Failed to read line");
-            if second_input == ("0\r\n")
-            {
-                println!("WARNING: Not recommended to go above 1,000,000 due to processing time (unless you're patient)");
-                println!("Origin it is");
-                println!("What radius circle?");
-                let val = input_value();
-                println!("Total lattice points in the circle: {:?}", origin_sum_below(val.clone()));
-            }
-            else
-            {
-                println!("WARNING: Not recommended to go above 1,000,000 due to processing time (unless you're patient)");
-                println!("1/2 then!");
-                println!("What radius circle?");
-                let val = input_value() * 2 + 1;
-                println!("Total lattice points on the circumference: {:?}", half_sum_below(val.clone()));
-            }
-    }
-    if first_input == ("circumference\r\n")
-    {
-        println!("Do you want the circle centered at x = 0 or x = 1/2? (y = 0 for both)");
-        let mut second_input = String::new();
-        io::stdin()
-            .read_line(&mut second_input)
-            .expect("Failed to read line");
-            if second_input == ("0\r\n")
-            {
-                println!("WARNING: Not recommended to go above 1,000,000 due to processing time (unless you're patient)");
-                println!("Origin it is");
-                println!("What radius circle?");
-                let val = input_value();
-                let fac_vec = factors(val.clone());
-                println!("Total lattice points on the circumference: {:?}", circum_lattice_points_origin(fac_vec.clone()));
-            }
-            else
-            {
-                println!("WARNING: Not recommended to go above 1,000,000 due to processing time (unless you're patient)");
-                println!("1/2 then!");
-                println!("What radius circle?");
-                let val = input_value() * 2 + 1;
-                let fac_vec = factors(val.clone());
-                println!("Total lattice points on the circumference: {:?}", circum_lattice_points_halfx(fac_vec.clone()));
-            }
-    }
+        if second_input == ("0\r\n")
+        {
+            println!("Origin it is");
+            println!("What radius circle?");
+            let val = input_value();
+            let fac_vec = factors(val.clone());
+            println!("Total lattice points on the circumference: {:?}", circum_lattice_points_origin(fac_vec.clone()));
+        }
+        else if second_input == ("1/2\r\n")
+        {
+            println!("1/2 then!");
+            println!("What radius circle?");
+            let val = input_value() * 2 + 1;
+            let fac_vec = factors(val.clone());
+            println!("Total lattice points on the circumference: {:?}", circum_lattice_points_halfx(fac_vec.clone()));
+        }
+        else
+        {
+            println!("Failed to read input. Please try again");
+            manage_first_input();
+        }
 }
 
 fn input_value() -> u64
@@ -84,28 +53,21 @@ fn input_value() -> u64
     return 1;
 }
 
-fn origin_sum_below(num: u64) -> u64
+fn input_float() -> f64
 {
-    let mut total_pts = 0;
-    let mut i = num;
-    while i > 0
-    {
-        total_pts += circum_lattice_points_origin(factors(i));
-        i -= 1;
-    }
-    return total_pts;
-}
+    let mut input = String::new();
 
-fn half_sum_below(num: u64) -> u64
-{
-    let mut total_pts = 0;
-    let mut i = num;
-    while i > 0
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let trimmed = input.trim();
+    match trimmed.parse::<f64>()
     {
-        total_pts += circum_lattice_points_halfx(factors(i));
-        i -= 1;
-    }
-    return total_pts;
+        Ok(i) =>  return i,
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+    return 1.0;
 }
 
 //***(0, 0) Code***
@@ -161,9 +123,9 @@ pub fn factors(mut n: u64) -> Vec<u64>
         }
         x += 2;
     }
-    
     factors
 }
+
 //***(1/2, 0) Code***
 fn int_sided_rtriangles(vec: Vec<u64>) -> u64
 {
